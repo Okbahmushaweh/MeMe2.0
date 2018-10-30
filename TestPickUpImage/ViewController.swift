922cfedadcf6dd19c931f2e6b5045843e186fd06
Okbah Mushaweh <okbahmushaweh@Okbahs-Mac.local>
2018-10-30 22:34:15 +0300

    Initial Commit

---
+++ TestPickUpImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestPickUpImage
+//
+//  Created by Okbah Mushaweh on 30/10/2018.
+//  Copyright © 2018 okbah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
